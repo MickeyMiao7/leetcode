8c6bdf1a165f5dcd61f6c21af53cc4c1b7cedee5
Mickey <327191467@qq.com>
2017-01-16 14:34:46 +0800

    66_plus_one

--- 66_plus_one.cpp
+++ 66_plus_one.cpp
@@ -5,10 +5,28 @@ using namespace std;
 
 class Solution{
     public:
-          
+          void plusOne(vector<int>& digits){
+              vector<int>::iterator iter;
+              for(int i=digits.size() -1; i>=0; i--){
+                  if(digits[i] == 9){
+                      digits[i] = 0;
+                  }
+                  else{
+                      digits[i] += 1;
+                      return;
+                  }
+              }
+              digits[0] = 1;
+              digits.push_back(0);
+              return;
+          }
 };
 
 int main(){
     Solution sol;
+    vector<int> input{1, 1, 9};
+    sol.plusOne(input);
+    for(int i =0; i < input.size(); i++)
+        cout << input[i] << endl; 
     return 0;
 }
